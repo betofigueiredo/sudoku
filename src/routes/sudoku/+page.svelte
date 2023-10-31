5e647eb29e0e6878b60ac10984594297d5e29953
Beto Figueiredo <beto@2sidesof1.com>
2023-10-31 00:14:58 -0300

    [feat] tailwind and first svelte component

---
+++ src/routes/sudoku/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import NumberButton from "./NumberButton.svelte";
+
+  let puzzle = "2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3";
+  let solution =
+    "284375196739816254651942378476128539312594687598637412143769825965283741827451963";
+
+  function isNumber(char: string): boolean {
+    return /^[0-9]+$/.test(char);
+  }
+
+  function keydown(event: KeyboardEvent) {
+    // if (event.metaKey) return;
+    if (!isNumber(event.key)) return;
+
+    console.log(event.key);
+
+    // if (event.key === "Enter") return;
+
+    // document
+    //   .querySelector(`[data-key="${event.key}" i]`)
+    //   ?.dispatchEvent(new MouseEvent("click", { cancelable: true }));
+  }
+</script>
+
+<svelte:window on:keydown={keydown} />
+
+<svelte:head>
+  <title>About</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="container">
+  <div class="grid grid-cols-9 w-[396px]">
+    {#each puzzle as item, idx}
+      <NumberButton {item} {idx} />
+    {/each}
+  </div>
+</div>
---
+++ src/routes/sudoku/NumberButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let item: string;
+  export let idx: number;
+
+  const row = 1; // TODO
+  const column = 1; // TODO
+  const block = 1; // TODO
+
+  function getBorder() {
+    let border = "w-11 h-11 border-l";
+    if ((idx + 0) % 3 === 0) border += " border-l-2 border-l-black";
+    if ((idx + 1) % 9 === 0) border += " border-r-2 border-r-black";
+    if (idx % 9 === 0) border += " border-l-2 border-l-black";
+    if (idx < 9) border += " border-t-2 border-t-black";
+    if (idx > 71) {
+      border += " border-b-2 border-b-black";
+    } else if (idx >= 18 && idx <= 26) {
+      border += " border-b-2 border-b-black";
+    } else if (idx >= 45 && idx <= 53) {
+      border += " border-b-2 border-b-black";
+    } else {
+      border += " border-b";
+    }
+    return border;
+  }
+</script>
+
+<button class={getBorder()}>
+  {item}
+</button>
