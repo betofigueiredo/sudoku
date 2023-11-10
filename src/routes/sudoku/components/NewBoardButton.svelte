ec56d377e315255649409c1db4c48991aa16ac28
Beto Figueiredo <beto@2sidesof1.com>
2023-11-09 22:26:00 -0300

    [feat] button component to create new boards

---
+++ src/routes/sudoku/components/NewBoardButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import store, { initPuzzle } from "../store";
+
+  const difficulties: { [k: string]: string } = {
+    Easy: "easy",
+    Medium: "medium",
+    Hard: "hard",
+    Expert: "very-hard",
+    Master: "insane",
+  };
+  export let label: string;
+  let difficulty: string | undefined = difficulties["Easy"];
+
+  store.subscribe((store) => {
+    difficulty = store.settings.difficulty;
+  });
+
+  function createNewBoard() {
+    initPuzzle(difficulties[label]);
+  }
+
+  $: isActive = difficulty === difficulties[label];
+</script>
+
+<button
+  type="button"
+  on:click={createNewBoard}
+  class={`pl-2 pr-2 ${isActive ? "text-[#3f9488]" : ""}`}>{label}</button
+>
