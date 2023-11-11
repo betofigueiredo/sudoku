63fe6455aa26598282e176cccbee0700159e26f8
Beto Figueiredo <beto@2sidesof1.com>
2023-11-10 21:07:46 -0300

    [fix] github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: undefined,
 			precompress: false,
 			strict: true
