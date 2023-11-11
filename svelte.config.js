4282cf75d5e40ddb8c1f28cbbe1575255da21613
Beto Figueiredo <beto@2sidesof1.com>
2023-11-10 21:31:15 -0300

    [fix] app paths for static images

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: "/sudoku"
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
@@ -18,7 +21,7 @@ const config = {
 			assets: 'docs',
 			fallback: undefined,
 			precompress: false,
-			strict: true
+			strict: true,
 		})
 	}
 };
